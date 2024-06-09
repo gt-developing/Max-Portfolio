28697347542c43ae79cf21d548a33bd46282b983
kairateadoro <kairateadoro@gmail.com>
2024-06-09 02:11:18 +0000

    icons and socials

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import "../css/presentation.css"
-
-
+import { FaGithub, FaInstagram } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 const Presentation = () => {
   return (
    
@@ -14,7 +14,14 @@ const Presentation = () => {
         <div className='info'>
             <h1>Hola, soy Max</h1>
             <p>Desarrollador web especializado en crear aplicaciones modernas y eficientes con React y Node.js. Apasionado por el diseño funcional y la experiencia del usuario.</p>
-            
+            <div className='redes'>
+              <a href="https://github.com/gt-developing"><FaGithub className='icon' /></a>
+              <a href="https://www.instagram.com/max_gtdev/"><FaInstagram className='icon' /></a>
+              <a target='_blank' href="mailto:balexismaximiliano@gmail.com"> <MdEmail className='icon'/></a>
+              
+              
+             
+            </div>
         </div>
 
     </div>
