df4aa03de5947d28d5fe3c8fe02781919ecfeed0
kairateadoro <kairateadoro@gmail.com>
2024-06-16 07:37:34 +0000

    version 2 omg

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -2,8 +2,14 @@ import React, { useEffect, useState } from 'react';
 import graffpreview from "../assets/graffpreview.png";
 import todopreview from "../assets/todopreview.png";
 import weatherpreview from "../assets/weatherpreview.png";
+import portfoliopreview from "../assets/portfoliopreview.png"
+import npmpreview from "../assets/npmpreview.png";
+
 import "../css/presentation.css";
-import { FaGithub, FaInstagram, FaExternalLinkAlt } from "react-icons/fa";
+import { FaPython, FaGithub, FaInstagram, FaExternalLinkAlt, FaReact, FaGitAlt, FaBootstrap, FaSass, FaHtml5, FaCss3, FaNodeJs } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { IoLogoFirebase, IoLogoJavascript } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
 
 // Importa el archivo JSON
@@ -13,7 +19,9 @@ import projectsData from '../json/projects.json';
 const images = {
   graffpreview,
   todopreview,
-  weatherpreview
+  weatherpreview,
+  npmpreview,
+  portfoliopreview
 };
 
 const Presentation = () => {
@@ -46,7 +54,7 @@ const Presentation = () => {
           <h1>Proyectos</h1>
           <p>Estos son algunos de los proyectos que he trabajado recientemente</p>
         </div>
-        
+
         <div className='projects-container'>
           {projects.map((project, index) => (
             <div className='card' key={index}>
@@ -60,12 +68,14 @@ const Presentation = () => {
                 </div>
                 <div className='date-links'>
                   <div className='links-app'>
-                    <a target='_blank' href={project.link}><FaExternalLinkAlt/></a>
+                    {project.link && (
+                      <a target='_blank' href={project.link}><FaExternalLinkAlt /></a>
+                    )}
                     {project.github && (
-                      <a target='_blank' href={project.github}><FaGithub/></a>
+                      <a target='_blank' href={project.github}><FaGithub /></a>
                     )}
                   </div>
-                  
+
                   <p>{project.year}</p>
                 </div>
               </div>
@@ -74,16 +84,71 @@ const Presentation = () => {
                   <p key={i}>{tag}</p>
                 ))}
               </div>
-             
+
             </div>
           ))}
         </div>
 
         <p className='more'>Encuentra aun mas en mi perfil de <a href="https://github.com/gt-developing">GitHub</a></p>
       </section>
-      <section className='about' id='about'>
-        <h1>Sobre mi</h1>
+
+      <section className='tech' id='tech'>
+
+       <div className='section-title'>
+        <h1>Tecnologias</h1>
+        <p>Habilidades</p>
+        </div>
+        <div className='tech-container'>
+
+          <div className='tech-card'>
+            <FaReact className='icon-card' />
+            <p>React</p>
+          </div>
+          <div className='tech-card'>
+            <FaBootstrap className='icon-card' />
+            <p>Bootstrap</p>
+          </div>
+          <div className='tech-card'>
+            <FaSass className='icon-card' />
+            <p>Sass</p>
+          </div>
+          <div className='tech-card'>
+            <FaHtml5 className='icon-card' />
+            <p>HTML5</p>
+          </div>
+          <div className='tech-card'>
+            <FaCss3 className='icon-card' />
+            <p>CSS3</p>
+          </div>
+          <div className='tech-card'>
+            <FaNodeJs className='icon-card' />
+            <p>Node.js</p>
+          </div>
+          <div className='tech-card'>
+            <IoLogoFirebase className='icon-card' />
+            <p>Firebase</p>
+          </div>
+          <div className='tech-card'>
+            <IoLogoJavascript className='icon-card' />
+            <p>Javascript</p>
+          </div>
+          <div className='tech-card'>
+            <RiTailwindCssFill className='icon-card' />
+            <p>Tailwind CSS</p>
+          </div>
+          <div className='tech-card'>
+            <FaPython className='icon-card' />
+            <p>Python</p>
+          </div>
+          <div className='tech-card'>
+            <GrMysql className='icon-card' />
+            <p>MySQL</p>
+          </div>
+
+
+        </div>
       </section>
+
       <section className='contact' id='contact'>
         <h1>Contacto</h1>
       </section>
